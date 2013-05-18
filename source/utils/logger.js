49b417000d42c41b4911e661ba536a6dd7085d99
alexander.beletsky <alexander.beletsky@gmail.com>
2013-05-18 13:48:29 +0300

    added logentries logging

--- source/utils/logger.js
+++ source/utils/logger.js
@@ -1,22 +1,30 @@
 var util = require('util');
 var colors = require('colors');
 var moment = require('moment');
+var logentries = require('node-logentries');
+var log = logentries.logger({
+	token:process.env.LOGENTRIES_TOKEN
+});
 
 module.exports = {
 	success: function (message) {
 		console.log(this.timestamptMessage(util.format('SUCCESS: %s', message)).green);
+		log.log('success', { message: message });
 	},
 
 	warning: function (message) {
 		console.log(this.timestamptMessage(util.format('WARNING: %s', message)).yellow);
+		log.log('warning', { message: message });
 	},
 
 	error: function (message) {
 		console.log(this.timestamptMessage(util.format('ERROR: %s', message)).red);
+		log.log('error', { message: message });
 	},
 
 	info: function (message) {
 		console.log(this.timestamptMessage(message));
+		log.info({ message: message });
 	},
 
 	connector: function (name) {
