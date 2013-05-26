34c361c39160b42a787c72a9ff7a9bb9048ecee9
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-05-26 07:43:31 +0300

    process crash logging

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ var path = require('path');
 var app = express();
 var engine = require('./source/engine').create();
 
+var logger = require('./source/utils/logger');
+
+process.on('uncaughtException', function (err) {
+	logger.error({msg:'Uncaught exception', error:err, stack:err.stack});
+	console.log("Uncaught exception", err, err.stack && err.stack.toString()); //extra log, makes stack track clickable in webstorm
+});
+
 app.configure(function(){
 	app.set('port', process.env.VCAP_APP_PORT || 3002);
 	app.set('views', __dirname + '/views');
@@ -22,7 +29,7 @@ app.configure('development', function(){
 
 http.createServer(app).listen(app.get('port'), function() {
 	var env = process.env.NODE_ENV || 'development';
-	console.log("likeastore-collector listening on port " + app.get('port') + ' ' + env);
+	logger.success("likeastore-collector listening on port " + app.get('port') + ' ' + env);
 
 	engine.start();
 });
