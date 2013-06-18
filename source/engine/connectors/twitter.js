814474481d507c60085992d66b5a04cd969ea175
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-06-18 22:26:30 +0300

    refactored twitter connector

--- source/engine/connectors/twitter.js
+++ source/engine/connectors/twitter.js
@@ -63,8 +63,8 @@ function connector(state, callback) {
 			state.mode = 'initial';
 		}
 
-		if (state.rateLimitExceed) {
-			delete state.rateLimitExceed;
+		if (state.mode === 'rateLimit') {
+			state.mode = state.prevMode;
 		}
 	}
 
@@ -72,20 +72,9 @@ function connector(state, callback) {
 		var rateLimit = +response.headers['x-rate-limit-remaining'];
 		log.info('rate limit remaining: ' + rateLimit + ' for user: ' + state.userId);
 
-		if (rateLimit === 0 || isNaN(rateLimit)) {
-			state.rateLimitExceed = true;
-			log.warning({message: 'rate limit exceeed', state: state});
-		}
-
 		if (!Array.isArray(body)) {
-			if (state.rateLimitExceed) {
-				logger.error({message: 'Unexpected response in rateLimitExceed mode (should not be possible)'});
-			}
-
 			return callback({ message: 'Unexpected response type', body: body, state: state});
 		}
-
-		state.lastestResponse = body;
 
 		var favorites = body.map(function (fav) {
 			return {
@@ -104,59 +93,34 @@ function connector(state, callback) {
 
 		log.info('retrieved ' + favorites.length + ' favorites');
 
-		return callback(null, updateState(state, favorites), favorites);
+		return callback(null, updateState(state, favorites, rateLimit), favorites);
 	}
 
-	function updateState(state, favorites) {
-		var stateChanges = [
-			// last execution
-			{
-				condition: function (state, data) {
-					return true;
-				},
-				apply: function (state, data) {
-					state.lastExecution = moment().format();
-				}
-			},
-			// intialize sinceId
-			{
-				condition: function (state, data) {
-					return state.mode === 'initial' && data.length > 0 && !state.sinceId;
-				},
-				apply: function (state, data) {
-					state.sinceId = data[0].itemId;
-				}
-			},
-			// store sinceId
-			{
-				condition: function (state, data) {
-					return state.mode === 'normal' && data.length > 0;
-				},
-				apply: function (state, data) {
-					state.sinceId = data[0].itemId;
-				}
-			},
-			{
-				condition: function (state, data) {
-					return state.mode === 'initial' && data.length > 0;
-				},
-				apply: function (state, data) {
-					state.maxId = helpers.decrementStringId(data[data.length - 1].itemId);
-				}
-			},
-			// go to normal
-			{
-				condition: function (state, data) {
-					return state.mode === 'initial' && data.length === 0;
-				},
-				apply: function (state, data) {
-					state.mode = 'normal';
-					delete state.maxId;
-				}
-			}
-		];
+	function updateState(state, data, rateLimit) {
+		if (state.mode === 'initial' && data.length > 0 && !state.sinceId) {
+			state.sinceId = data[0].itemId;
+		}
 
-		return stater.update(state, stateChanges, favorites);
+		if (state.mode === 'normal' && data.length > 0) {
+			state.sinceId = data[0].itemId;
+		}
+
+		if (state.mode === 'initial' && data.length > 0) {
+			state.maxId = helpers.decrementStringId(data[data.length - 1].itemId);
+		}
+
+		if (state.mode === 'initial' && data.length === 0) {
+			state.mode = 'normal';
+			delete state.maxId;
+		}
+
+		if (rateLimit === 1) {
+			var currentState = state.mode;
+			state.mode = 'rateLimit';
+			state.prevMode = currentState;
+		}
+
+		return state;
 	}
 }
 
