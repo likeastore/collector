b3e30f018bae5f337238741ef1840477b552e564
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-07-20 14:14:52 +0300

    minor

--- cleanDb.js
+++ cleanDb.js
@@ -1,6 +1,6 @@
 var db = require('./source/db/dbConnector').db;
 
-console.log('cleaning up likestore db...')
+console.log('cleaning up likestore db...');
 db.dropDatabase();
 db.close(function (err) {
 	if (err) {
