070725ab53380781e49854e9bcb4f08b0716be66
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-07-31 06:27:12 +0300

    collector main

---
+++ source/collector.js
@@ -0,0 +1,20 @@
+var argv = require('optimist').argv;
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.COLLECTOR_MODE = process.env.COLLECTOR_MODE || argv.mode || 'normal';
+
+var config = require('../config');
+var logger = require('./utils/logger');
+var connectors = require('./engine/connectors');
+var scheduler = require('./engine/scheduler');
+
+process.on('uncaughtException', function (err) {
+	logger.fatal({msg:'Uncaught exception', error:err, stack:err.stack});
+	console.log("Uncaught exception", err, err.stack && err.stack.toString()); //extra log, makes stack track clickable in webstorm
+});
+
+var env = process.env.NODE_ENV;
+var mode = process.env.COLLECTOR_MODE;
+logger.success('likeastore-collector started env:' + env + ' mongodb: ' + config.connection + ' mode: ' + mode);
+
+scheduler.run(mode, connectors);
