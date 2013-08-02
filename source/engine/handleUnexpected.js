5674bb60084282eeaeeeacf36e2604e6a7b6a716
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-08-02 15:49:13 +0300

    simplified error handler

--- source/engine/handleUnexpected.js
+++ source/engine/handleUnexpected.js
@@ -7,25 +7,11 @@ function handleUnexpected(response, body, state, callback) {
 	state.errors += 1;
 
 	if (state.errors === MAX_ERRORS_ALLOWED) {
-		return users.notifyOnError(state.user, disableNetwork);
-	}
-
-	function disableNetwork (err) {
-		if (err) {
-			return callback({message: 'Failed to notify user', err: err});
-		}
-
 		state.errors = 0;
 		state.disabled = true;
-
-		returnError();
 	}
 
-	function returnError () {
-		callback({ message: 'Unexpected body type', body: body, status: response ? response.statusCode : body.error_id});
-	}
-
-	disableNetwork(null);
+	callback({ message: 'Unexpected body type', body: body, status: response ? response.statusCode : body.error_id});
 }
 
 module.exports = handleUnexpected;
