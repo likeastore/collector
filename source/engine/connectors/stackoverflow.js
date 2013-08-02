cb6bf880359096d14ce98f62b4e37d5c0a4b1482
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-08-02 16:38:56 +0300

    fixed and tested

--- source/engine/connectors/stackoverflow.js
+++ source/engine/connectors/stackoverflow.js
@@ -48,6 +48,10 @@ function connector(state, callback) {
 			state.mode = 'initial';
 		}
 
+		if (!state.errors) {
+			state.errors = 0;
+		}
+
 		if (state.mode === 'initial' && !state.page) {
 			state.page = 1;
 		}
--- source/engine/handleUnexpected.js
+++ source/engine/handleUnexpected.js
@@ -1,17 +1,22 @@
 var users = require('../db/users');
 var util = require('util');
+var logger = require('../utils/logger');
 
 var MAX_ERRORS_ALLOWED = 3;
 
 function handleUnexpected(response, body, state, callback) {
 	state.errors += 1;
 
+	logger.warning({message: 'Unexpected response.', body: body, status: response ? response.statusCode : body.error_id,})
+
 	if (state.errors === MAX_ERRORS_ALLOWED) {
-		state.errors = 0;
+		delete state.errors;
 		state.disabled = true;
+
+		return callback({ message: 'Connector disabled, due to max errors count.', body: body, status: response ? response.statusCode : body.error_id});
 	}
 
-	callback({ message: 'Unexpected body type', body: body, status: response ? response.statusCode : body.error_id});
+	callback(null);
 }
 
 module.exports = handleUnexpected;
