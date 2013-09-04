f043e70574a234b26332259bf91eb41454ccbf22
Ingvar Stepanyan <me@rreverser.com>
2013-09-04 19:21:07 +0300

    Added retweets connector. Moved twitter code to common library for both favorite tweets and retweets connectors.

--- source/engine/connectors/index.js
+++ source/engine/connectors/index.js
@@ -1,6 +1,7 @@
 module.exports = {
 	github: require('./github'),
 	twitter: require('./twitter'),
+	retweets: require('./retweets'),
 	stackoverflow: require('./stackoverflow'),
 	gist: require('./gist'),
 	facebook: require('./facebook')
---
+++ source/engine/connectors/retweets.js
@@ -0,0 +1,8 @@
+module.exports = require('./twitter.base').bind({
+	uri: '/statuses/user_timeline.json?exclude_replies=true',
+	preprocess: function (tweets) {
+		return tweets
+			   .map(function (tweet) { return tweet.retweeted_status })
+			   .filter(Boolean);
+	}
+});	
