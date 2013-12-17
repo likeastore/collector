c6736f4b6d5b056911f2c14999a4647777d16cf3
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-17 15:38:47 +0200

    make collector voice a bit low..

--- source/engine/handleUnexpected.js
+++ source/engine/handleUnexpected.js
@@ -2,7 +2,7 @@ var users = require('../db/users');
 var util = require('util');
 var logger = require('../utils/logger');
 
-var MAX_ERRORS_ALLOWED = 3;
+var MAX_ERRORS_ALLOWED = 5;
 
 function handleUnexpected(response, body, state, err, callback) {
 	if (typeof err === 'function') {
@@ -12,8 +12,6 @@ function handleUnexpected(response, body, state, err, callback) {
 	state.errors += 1;
 
 	var status = response ? response.statusCode : body ? body.error_id : err;
-	logger.warning({message: 'Unexpected response.', body: body, status: status});
-
 	if (state.errors === MAX_ERRORS_ALLOWED) {
 		delete state.errors;
 		state.disabled = true;
