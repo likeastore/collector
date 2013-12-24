6964dc3c796eb6a2b28d44c9c284b98a76ff8686
alexanderbeletsky <alexander.beletsky@gmail.com>
2013-12-24 14:52:34 +0200

    find and cache

--- source/models/users.js
+++ source/models/users.js
@@ -1,8 +1,23 @@
+var _ = require('underscore');
 var config = require('../../config');
 var db = require('../db')(config);
 
+var usersCache = {};
+
 module.exports = {
-	notifyOnError: function (user, message, callback) {
-		db.users.findAndModify({user: user}, [], {$set: {warning: true}}, {}, callback);
+	findAndCache: function (email, callback) {
+		if (usersCache[email]) {
+			return callback (null, usersCache[email]);
+		}
+
+		db.users.findOne({email: email}, function (err, user) {
+			if (err) {
+				return callback(err);
+			}
+
+			usersCache[email] = _.pick(user, ['avatar', 'displayName', 'email', 'name' ]);
+
+			return callback (null, usersCache[email]);
+		});
 	}
 };
