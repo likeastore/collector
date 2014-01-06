f33783698f0406d08206792806e3feec20f6e402
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-01-06 11:04:47 +0200

    behance dump

---
+++ source/engine/connectors/behance.js
@@ -0,0 +1,119 @@
+var request = require('request');
+var moment = require('moment');
+var util = require('util');
+
+var scheduleTo = require('../scheduleTo');
+var logger = require('./../../utils/logger');
+var handleUnexpected = require('../handleUnexpected');
+var config = require('../../../config');
+
+var API = 'https://www.behance.net/v2';
+
+function connector(state, callback) {
+	var accessToken = state.accessToken;
+	var log = logger.connector('behance');
+
+	if (!accessToken) {
+		return callback('missing accessToken for user: ' + state.user);
+	}
+
+	initState(state);
+
+	log.info('prepearing request in (' + state.mode + ') mode for user: ' + state.user);
+
+	var uri = formatRequestUri(accessToken, state);
+	var headers = { 'Content-Type': 'application/json', 'User-Agent': 'likeastore/collector'};
+
+	request({uri: uri, headers: headers, timeout: config.collector.request.timeout, json: true}, function (err, response, body) {
+		if (err) {
+			return handleUnexpected(response, body, state, err, function (err) {
+				callback (err, state);
+			});
+		}
+
+		return handleResponse(response, body);
+	});
+
+	function initState(state) {
+		if (!state.mode) {
+			state.mode = 'initial';
+		}
+
+		if (!state.errors) {
+			state.errors = 0;
+		}
+
+		if (state.mode === 'initial' && !state.page) {
+			state.page = 1;
+		}
+
+		if (state.mode === 'rateLimit') {
+			state.mode = state.prevMode;
+		}
+	}
+
+	function formatRequestUri(accessToken, state) {
+		var base = util.format('%s/users/starred?access_token=%s&per_page=100', API, accessToken);
+		return state.mode === 'initial' || state.page ?
+			util.format('%s&page=%s', base, state.page) :
+			base;
+	}
+
+	function handleResponse(response, body) {
+		var rateLimit = +response.headers['x-ratelimit-remaining'];
+		log.info('rate limit remaining: ' +  rateLimit + ' for user: ' + state.user);
+
+		if (!Array.isArray(body)) {
+			return handleUnexpected(response, body, state, function (err) {
+				callback(err, scheduleTo(updateState(state, [], rateLimit, true)));
+			});
+		}
+
+		var stars = body.map(function (r) {
+			return {
+				itemId: r.id.toString(),
+				idInt: r.id,
+				user: state.user,
+				name: r.full_name,
+				repo: r.name,
+				authorName: r.owner.login,
+				authorUrl: r.owner.html_url,
+				authorGravatar: r.owner.gravatar_id,
+				avatarUrl: 'https://www.gravatar.com/avatar/' + r.owner.gravatar_id + '?d=mm',
+				source: r.html_url,
+				created: moment(r.created_at).toDate(),
+				description: r.description,
+				type: 'github'
+			};
+		});
+
+		log.info('retrieved ' + stars.length + ' stars for user: ' + state.user);
+
+		return callback(null, scheduleTo(updateState(state, stars, rateLimit, false)), stars);
+	}
+
+	function updateState(state, data, rateLimit, failed) {
+		state.lastExecution = moment().toDate();
+
+		if (!failed) {
+			if (state.mode === 'initial' && data.length > 0) {
+				state.page += 1;
+			}
+
+			if (state.mode === 'initial' && data.length === 0) {
+				state.mode = 'normal';
+				delete state.page;
+			}
+
+			if (rateLimit <= 1) {
+				var currentState = state.mode;
+				state.mode = 'rateLimit';
+				state.prevMode = currentState;
+			}
+		}
+
+		return state;
+	}
+}
+
+module.exports = connector;
