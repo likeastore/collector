aee6e83e9c7f80bcb146ffe9cd1e88e934b63a42
alexanderbeletsky <alexander.beletsky@gmail.com>
2014-06-04 11:34:01 +0300

    corrected

--- source/models/users.js
+++ source/models/users.js
@@ -4,7 +4,7 @@ var moment = require('moment');
 var config = require('../../config');
 var db = require('../db')(config);
 
-var userPickFields = ['_id', 'avatar', 'bio', 'displayName', 'email', 'location', 'username', 'website'];
+var userPickFields = ['_id', 'avatar', 'bio', 'displayName', 'email', 'location', 'name', 'username', 'website'];
 
 function findByEmail(email, callback) {
 	db.users.findOne({email: email}, function (err, user) {
